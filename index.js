25fc266cf4d843d455d44c8422afb06df6235167
aditya-qlik <aditya.kumar@qliktag.com>
2020-02-20 13:58:55 +0530

    task1 part b done

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const number1 = document.getElementById('num1');
 const number2 = document.getElementById('num2');
 const operation = document.querySelector('#operator');
 const submit = document.querySelector('[name="submit"]');
-
+let num1;
+let num2;
+let printOut;
 function add(valueOfNum1 = 0, valueOfNum2 = 0) {
         return valueOfNum1 + valueOfNum2;
 }
@@ -15,63 +17,77 @@ function multiplication(valueOfNum1 = 0, valueOfNum2 = 1) {
         return valueOfNum1 * valueOfNum2;
 }
 function division(valueOfNum1 = 0, valueOfNum2 = 1) {
-        return valueOfNum1 / valueOfNum2;
+        if (num2 === 0) {
+                console.error(`2nd Number can't be 0`);
+        } else {
+                return valueOfNum1 / valueOfNum2;
+        }
 }
-let num1;
-let num2;
-function checkNumber1(event) {
-        num1 = parseFloat(event.target.value);
+function modulus(valueOfNum1 = 0, valueOfNum2 = 1) {
+        return valueOfNum1 % valueOfNum2;
+}
+function displayResult(toBeDisplayed) {
+        document.querySelector('#displayResult').innerHTML = toBeDisplayed;
+}
+function checkNumber1() {
+        num1 = parseFloat(number1.value);
         const valueOfNumber1 = number1.value;
-        // console.log(num1);
         if (valueOfNumber1 === '') {
                 num1 = undefined;
         } else if (isNaN(num1)) {
-                alert(`enter number only`);
+                console.error(`Enter a NUMBER `);
                 number1.value = '';
                 num1 = undefined;
         }
 }
-function checkNumber2(event) {
-        num2 = parseFloat(event.target.value);
+function checkNumber2() {
+        num2 = parseFloat(number2.value);
         const valueOfNumber2 = number2.value;
-        // console.log(num2);
         if (valueOfNumber2 === '') {
                 num2 = undefined;
         } else if (isNaN(num2)) {
-                alert(`enter number only`);
+                console.error(`Enter a NUMBER`);
                 number2.value = '';
                 num2 = undefined;
         }
 }
 number1.addEventListener('blur', checkNumber1);
 number2.addEventListener('blur', checkNumber2);
-function printResult() {
+function calculate() {
+        checkNumber1();
+        checkNumber2();
         const operator = operation.value;
         // console.log(num1, num2);
         switch (operator) {
                 case '+':
-                        console.log(`the result is ${add(num1, num2)}`);
+                        printOut = `The sum of ${num1} and ${num2} is ${add(num1, num2)}`;
                         break;
                 case '-':
-                        console.log(`the result is ${subtraction(num1, num2)}`);
+                        printOut = `The difference of ${num1} and ${num2} is ${subtraction(num1, num2)}`;
                         break;
                 case '*':
-                        console.log(`the result is ${multiplication(num1, num2)}`);
+                        printOut = `The product of ${num1} and ${num2} is ${multiplication(num1, num2)}`;
                         break;
                 case '/':
-                        if (num2 === 0) {
-                                alert(`2nd Number can't be 0`);
-                        } else {
-                                console.log(`the result is ${division(num1, num2)}`);
-                        }
+                        printOut = `The quotient of ${num1} and ${num2} is ${division(num1, num2)}`;
+                        break;
+                case '%':
+                        printOut = ` The modulus of ${num1} and ${num2} is ${modulus(num1, num2)}`;
+                        break;
+                default:
+                        printOut = `Invalid operator`;
         }
+        displayResult(printOut);
 }
-submit.addEventListener('click', printResult);
+
+submit.addEventListener('click', calculate);
 function clear() {
         console.clear();
         number1.value = '';
         number2.value = '';
+        document.querySelector('#displayResult').innerHTML = '';
 }
 document.querySelector('#clean').addEventListener('click', clear);
+
 
 
